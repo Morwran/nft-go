3c65087aed5c89afd0afa4ebfab51a13e75271d4
Vadim <mat3x@mail.ru>
2025-05-26 20:10:54 +0300

    export some constants (#4)

--- pkg/nftenc/expr.go
+++ pkg/nftenc/expr.go
@@ -6,3 +6,18 @@ type (
 	// Export some internal types
 	VerdictKind = expr.VerdictKind
 )
+
+const (
+	// Export some internal constants
+	VerdictReturn   = expr.VerdictReturn
+	VerdictGoto     = expr.VerdictGoto
+	VerdictJump     = expr.VerdictJump
+	VerdictBreak    = expr.VerdictBreak
+	VerdictContinue = expr.VerdictContinue
+	VerdictDrop     = expr.VerdictDrop
+	VerdictAccept   = expr.VerdictAccept
+	VerdictStolen   = expr.VerdictStolen
+	VerdictQueue    = expr.VerdictQueue
+	VerdictRepeat   = expr.VerdictRepeat
+	VerdictStop     = expr.VerdictStop
+)
